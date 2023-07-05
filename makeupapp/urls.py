7556748b6dfc2d0a559f4384fbafbb51479474ed
Sree Rajendran <sreerajendran@Srees-MacBook-Air.local>
2023-07-05 18:08:23 +0400

    Add changes for adding products to cart

--- makeupapp/urls.py
+++ makeupapp/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('index/', views.ok1, name='index'),
     path('sproduct/', views.ok2, name='sproduct'),
     path('cart/', views.ok3, name='cart'),
+    path('login_page/', views.loginaction, name='login_page'),
+    path('login_page/', views.ok4, name='login_page'),
+    path('signup_page/', views.signupaction, name='signup_page'),
+    path('signup_page/', views.ok5, name='signup_page'),
 ]
--- makeupapp/views.py
+++ makeupapp/views.py
@@ -1,5 +1,57 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import mysql.connector
+
+#signup page variables
+fn=''
+ln=''
+em=''
+pwd=''
+
+def signupaction(request):
+    global nm,em,pwd,bday
+    if request.method=="POST":
+        m = mysql.connector.connect(host="localhost", user="root", passwd="pepperoni@pizzA1", database="makeup")
+        cursor=m.cursor()
+        d=request.POST
+        for key,value in d.items(): 
+            if key=="fname":
+                fn=value
+            if key=="lname":
+                ln=value 
+            if key=="email":
+                em=value
+            if key=="password":
+                pwd=value           
+        c = "INSERT INTO users (firstname, lastname, emailid, password) VALUES ('{}', '{}', '{}', '{}')".format(fn, ln, em, pwd)
+        cursor.execute(c)
+        m.commit()
+
+
+    return render(request,'signup_page.html')  
+
+def loginaction(request):
+    global em,pwd
+    if request.method=="POST":
+        m = mysql.connector.connect(host="localhost", user="root", passwd="pepperoni@pizzA1", database="makeup")
+        cursor=m.cursor()
+        d=request.POST
+        for key,value in d.items(): 
+            if key=="email":
+                em=value
+            if key=="password":
+                pwd=value           
+        c = "SELECT * from users where emailid='{}' and password='{}'".format(em, pwd)
+        cursor.execute(c)
+        t=tuple(cursor.fetchall())
+        if t==():
+            return render(request,'error.html')
+        else:
+            return render(request,'index.html')
+    return render(request,'login_page.html')  
+
+
+
 
 def ok(request):
     return render(request,'shop.html')
@@ -9,3 +61,7 @@ def ok2(request):
     return render(request,'sproduct.html')  
 def ok3(request):
     return render(request,'cart.html')  
+def ok4(request):
+    return render(request,'login_page.html') 
+def ok5(request):
+    return render(request,'signup_page.html') 
