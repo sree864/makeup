d2a83f27420aec6bbc06ef114d0ff5ee1c38b6e7
Sreelakshmi Rajendran <82442331+sree864@users.noreply.github.com>
2023-08-13 14:51:16 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -8,8 +8,6 @@ shape_predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
 # Load the lipstick image
 lipstick_image = cv2.imread('l1.png')
-
-# Create a mask to remove the white background from the lipstick image
 lipstick_mask = cv2.cvtColor(lipstick_image, cv2.COLOR_BGR2GRAY)
 _, lipstick_mask = cv2.threshold(lipstick_mask, 1, 255, cv2.THRESH_BINARY)
 lipstick_mask = cv2.bitwise_not(lipstick_mask)
@@ -21,11 +19,7 @@ cap = cv2.VideoCapture(0)
 Z = lipstick_image.reshape((-1, 3)).astype(np.float32)
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 K = 5  # Number of clusters
-_, _, centers = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-
-# Get the dominant red shade from the centers
-dominant_red = centers[:, 2].argmax()
-dominant_red_color = tuple(map(int, centers[dominant_red]))
+_, labels, centers = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
 while True:
     # Read a frame from the camera
@@ -37,47 +31,27 @@ while True:
     # Detect faces in the grayscale frame
     faces = face_detector(gray)
 
-    # Iterate over the detected faces
     for face in faces:
-        # Predict the facial landmarks
         shape = shape_predictor(gray, face)
         landmarks = np.array([(shape.part(i).x, shape.part(i).y) for i in range(shape.num_parts)], np.int32)
 
-        # Extract the lip region
         lips_region = cv2.convexHull(landmarks[48:61])
         (x, y, w, h) = cv2.boundingRect(lips_region)
 
-        # Resize the lipstick mask to match the lip region size
         lipstick_mask_resized = cv2.resize(lipstick_mask, (w, h))
 
-        # Apply the mask to the lipstick image
-        lipstick_roi = cv2.bitwise_and(lipstick_image[y:y + h, x:x + w], lipstick_image[y:y + h, x:x + w], mask=lipstick_mask_resized)
+        # Find the cluster index that corresponds to a shade of red with low G and B values
+        red_cluster = None
+        for i, center in enumerate(centers):
+            b, g, r = center
+            if r > 110 and g < 70 and b < 70:  # Adjust these thresholds as needed
+                red_cluster = i
+                break
 
-        # Convert the lipstick ROI to HSV color space
-        hsv_lipstick_roi = cv2.cvtColor(lipstick_roi, cv2.COLOR_BGR2HSV)
-
-        # Create a mask for the red shades
-        lower_red1 = np.array([0, 100, 100])
-        upper_red1 = np.array([10, 255, 255])
-        lower_red2 = np.array([170, 100, 100])
-        upper_red2 = np.array([180, 255, 255])
-        mask1 = cv2.inRange(hsv_lipstick_roi, lower_red1, upper_red1)
-        mask2 = cv2.inRange(hsv_lipstick_roi, lower_red2, upper_red2)
-        mask = cv2.bitwise_or(mask1, mask2)
-
-        # Count the number of pixels in the red shades
-        pixel_count = np.sum(mask > 0)
-
-        # If there are red pixels, calculate the average color
-        if pixel_count > 0:
-            average_color = cv2.mean(lipstick_roi, mask=mask)[:3]
-            average_color = tuple(map(int, average_color))
-        else:
-            # Use the dominant red shade if no red pixels are found
-            average_color = dominant_red_color
-
-        # Apply the extracted average color to the lips region
-        cv2.fillPoly(frame, [lips_region], average_color)
+        if red_cluster is not None:
+            # Apply the color from the red_cluster to the lips region (in BGR format)
+            color = [int(val) for val in centers[red_cluster]]  # Reverse the order to BGR
+            cv2.fillPoly(frame, [lips_region], color)
 
         # Draw a rectangle around the face
         (x, y, w, h) = (face.left(), face.top(), face.width(), face.height())
